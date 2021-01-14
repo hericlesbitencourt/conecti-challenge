c9a7b97ed3f75e34e8ebc9c9b0840b51387d9197
Hericles Bitencourt <hericlesbr@gmail.com>
2021-01-14 11:10:14 -0300

    Add Cors Headers

--- src/containers/CompanyList.js
+++ src/containers/CompanyList.js
@@ -30,6 +30,8 @@ const App = () => {
   } = useForm({
     resolver: yupResolver(schema),
   });
+  axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+  axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
   useEffect(() => {
     axios
